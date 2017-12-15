fee8e4548410f53f02994692a2435a70e76807a3
swiftyy188 <jeffho.ml@gmail.com>
2017-12-14 20:22:30 -0500

    routes, html&css, bootstrap link added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
 
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DealsComponent } from './deals/deals.component';
-import { CallbackComponent } from './callback/callback.component';
+import { CallbackComponent } from './callback.component';
 
+const appRoutes: Routes = [
+
+  {
+    path: 'deals',
+    component: DealsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'deals',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +27,12 @@ import { CallbackComponent } from './callback/callback.component';
     CallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } //debugging purpose only
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
